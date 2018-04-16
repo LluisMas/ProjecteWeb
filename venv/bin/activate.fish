b5b7918ed2ac3707a1731e279416fa69be818243
Lluis Mas <lmr14@alumnes.udl.cat>
2018-04-16 17:15:23 +0200

    Gitignore

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/lmr/Desktop/web/LRLdistributors/venv"
+set -gx VIRTUAL_ENV "/home/lmr/Desktop/LRLdistributors/ProjecteWeb/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
