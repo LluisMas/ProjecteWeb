581b225f7c3fe871cfb3c077e88695ba686c21d9
Lluis Mas <lmr14@alumnes.udl.cat>
2018-05-15 10:54:40 +0200

    Added geckodriver, changed readme, deleted duplicates from urls.py and kinda fixed register_dealership test.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 LRL Distributors
+
+If you want to test in Ubuntu, we've not found how to do it in Chrome so will be in Firefox, geckodriver is in the root folder of the project, all you have to do to make it work is add "geckodriver" to the PATH.
+
--- features/environment.py
+++ features/environment.py
@@ -4,6 +4,7 @@ from behave.runner import Context
 from django.shortcuts import resolve_url
 from django.test.runner import DiscoverRunner
 from django.test.testcases import LiveServerTestCase
+from selenium.webdriver.firefox import webdriver
 from splinter.browser import Browser
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "distributorsapp.settings"
@@ -19,8 +20,7 @@ def before_all(context):
     django.setup()
     context.test_runner = DiscoverRunner()
     context.test_runner.setup_test_environment()
-    context.browser = Browser('chrome', headless=True)
-
+    context.browser = Browser('firefox', headless=True)
 
 def before_scenario(context, scenario):
     context.old_db_config = context.test_runner.setup_databases()
