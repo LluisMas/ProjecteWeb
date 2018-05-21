9d37936ee3628e8d3efd5387387fb6b67c70f67c
radu <rgoldentime@gmail.com>
2018-05-21 18:24:55 +0200

    Carshops must not be removed

---
+++ distributors/static/js/images.js
@@ -0,0 +1,4 @@
+$(document).ready(function()
+{
+    document.getElementById("i1").src="https://ep01.epimg.net/economia/imagenes/2017/09/15/actualidad/1505491432_795544_1505493685_noticia_normal.jpg";
+});
