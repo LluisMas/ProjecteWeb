8c6532d10a98af4fd1adf81a71ebd2f01048cf1f
radu <rgoldentime@gmail.com>
2018-05-23 21:29:29 +0200

    Add car test partial implemented

--- features/register_car.feature
+++ features/register_car.feature
@@ -1,36 +1,38 @@
-Feature: Register Dealership
+
+Feature: Register Car
   # Enter feature description here
-  #
-  #
-  Background: There is a registered user and restaurant
+
+  Background: There is a registered user
     Given Exists a user "user" with password "password" and email "test@test.com" with Seller role
     Given Exists a user "customer" with password "password" and email "customer@customer.com" with Customer role
+    And Exists a carshop with name "shopName"
 
 
-  Scenario: Register a Dealership
+  Scenario: Register just Car
     Given I login as user "test@test.com" with password "password" and I am user "user"
-    When I register dealership
-      | shopName       |
+    When I register a car at carshop "shopName"
+      | name       |
       | Fish and Chips |
-    Then I'm viewing the details page for dealership by "test@test.com"
-      | shopName       |
+    Then I'm viewing the details page for car at carshop "shopName" by "user"
+      | name       |
       | Fish and Chips |
-    And There are 1 dealerships
+    And There are 1 cars
 
-  Scenario: Register a Dealership with Customer role
+  Scenario: Register a Car with Customer role
     Given I login as user "customer@customer.com" with password "password" and I am user "customer"
-    When I register dealership
-      | shopName       |
+    When I register car
+      | name       |
       | Fish and Chips |
-    And There are 0 dealerships
+    And There are 0 cars
 
 
-  Scenario: Register a Dealership two times
+  Scenario: Register a Car two times
     Given I login as user "test@test.com" with password "password" and I am user "user"
-    When I register dealership
-      | shopName       |
+    When I register car
+      | name       |
       | Fish and Chips |
-    When I register dealership
-      | shopName       |
+    When I register car
+      | name       |
       | Fish and Chips |
-    And There are 1 dealerships
+    And There are 1 cars
+
---
+++ features/steps/register_car.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+from behave import *
+import operator
+from django.db.models import Q
+from django.urls.base import reverse
+
+
+
+
+use_step_matcher("parse")
+
+@step('Exists a carshop with name "{shopName}"')
+def step_impl(context, shopName):
+    from distributors.models import Person
+    user = Person.objects.create_user(name="user", email="email", password="password", type=2)
+    from distributors.models import CarShop
+    CarShop.objects.create(inaugurationYear="1997", shopName=shopName, addr="Laverga",
+                           country="Argentina", city="Franco", user=user)
+
+
+@when('I register a car at carshop "{shopName}"')
+def step_impl(context, shopName):
+    from distributors.models import CarShop
+    carShop = CarShop.objects.get(shopName=shopName)
+
+
+    for row in context.table:
+        context.browser.visit(context.get_url('distributors:add_car', carShop.pk))
+        if context.browser.url == context.get_url('distributors:add_car', carShop.pk):
+            form = context.browser.find_by_tag('form').first
+            context.browser.fill("name","AnyShop")
+            context.browser.fill("kms", "AnyKms")
+            context.browser.fill("price", "AnyPrice")
+            form.find_by_css('button.btn-success').first.click()
+
+@then('I\'m viewing the details page for car at carshop "{shopName}" by "{user}"')
+def step_impl(context, shopName, user):
+    from distributors.models import CarShop
+    carShop = CarShop.objects.get(shopName=shopName)
+
+    q_list = [Q((attribute, context.table.rows[0][attribute])) for attribute in context.table.headings]
+    from distributors.models import Person
+    q_list.append(Q(('user', Person.objects.get(name=user))))
+
+    q_list.append(Q(('carshop', carShop)))
+
+    from distributors.models import Car
+    car = Car.objects.filter(reduce(operator.and_, q_list)).get()
+    with open('some.txt', 'a') as the_file:
+     the_file.write(context.browser.url)
+     the_file.write('\n')
+     the_file.write(context.get_url(car))
+    assert context.browser.url == context.get_url(car)
+
+    from distributors.models import Car
+    #car = Car.objects.get(shopName=carShop.shopName)
+
+    #with open('some.txt', 'a') as the_file:
+    #    the_file.write('Hello\n')
+
+    #assert context.browser.url == context.get_url(car)
+
+
+
+@step("There are {count:n} cars")
+def step_impl(context, count):
+    from distributors.models import Car
+    assert count == Car.objects.count()
+
+
+@step('Exists cars registered by "{username}"')
+def step_impl(context, username):
+    from distributors.models import Person
+    user = Person.objects.get(email=username)
+    from distributors.models import Car
+    for row in context.table:
+        car = Car(user=user)
+        for heading in row.headings:
+            setattr(car, heading, row[heading])
+        car.save()
