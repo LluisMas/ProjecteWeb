f8c615b5589dd7a6fa3f32f2ab18e1031a6c3e40
Lluis Mas <lmr14@alumnes.udl.cat>
2018-05-23 10:06:51 +0200

    Carshop list test and fixes

--- features/steps/create_sell.py
+++ features/steps/create_sell.py
@@ -6,25 +6,18 @@ use_step_matcher("parse")
 @step('Exists a car with name "{car}"')
 def step_impl(context, car):
     from distributors.models import Car
-    Car.objects.create()
-    pass
+    Car.objects.create(name=car, body="sedan")
 
 
-@step('Exists a Seller with name "{seller}"')
-def step_impl(context, seller):
-    """
-    :type seller: str
-    :type context: behave.runner.Context
-    """
-    pass
-
-
-@when("I register sell")
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    pass
+@when('I register sell for car "{car}"')
+def step_impl(context, car):
+    from distributors.models import Car
+    car = Car.objects.get(name=car)
+    for row in context.table:
+        context.browser.visit(context.get_url('distributors:add_sell', car.pk))
+        if context.browser.url == context.get_url('distributors:add_distributors'):
+            form = context.browser.find_by_tag('form').first
+            form.find_by_css('button.btn-success').first.click()
 
 
 @then("I'm viewing the details of the sell")
