9bcdffdfaf192690b82925f817086df3c395ad5f
Lluis Mas <lmr14@alumnes.udl.cat>
2018-05-24 21:57:18 +0200

    Review dealership test finished

--- features/review_dealership.feature
+++ features/review_dealership.feature
@@ -1,4 +1,3 @@
-# Created by lmr at 24/05/18
 Feature: Register Review
   In order to share my opinion about a dealership
   As a user
@@ -15,3 +14,29 @@ Feature: Register Review
     | rating          | comment                          |
     | 4               | Best stolen cars in the city     |
     Then I'm viewing the details page for dealership with name "PawnShop"
+    And I'm viewing the reviews containing
+    | rating          | comment                          |  user      |
+    | 4               | Best stolen cars in the city     |  customer  |
+    And list contains "1" review
+    And There are "1" reviews
+
+
+    Scenario: Try to register review but not logged
+      Given I'm not logged in
+      Then There is not "Add Review" link available
+      And There are "0" reviews
+
+
+    Scenario: Doing another review replaces the previous
+      Given Exists review at dealership "PawnShop" by "customer"
+      | rating          | comment                          |
+      | 4               | Best stolen cars in the city     |
+      And I login as user "customer@customer.com" with password "customer" and I am user "customer"
+      When I register a review at carshop "PawnShop"
+      | rating          | comment                          |
+      | 1               | One worker harassed me           |
+      Then I'm viewing the details page for dealership with name "PawnShop"
+      And I'm viewing the reviews containing
+      | rating          | comment                          |  user      |
+      | 1               | One worker harassed me           |  customer  |
+
--- features/steps/review_dealership.py
+++ features/steps/review_dealership.py
@@ -22,3 +22,44 @@ def step_impl(context, email, shop_name):
     carshop = CarShop(shopName=shop_name, user=user)
     carshop.addr="anyAddress"
     carshop.save()
+
+
+@step("I'm viewing the reviews containing")
+def step_impl(context):
+    reviews = context.browser.find_by_css('blockquote.review')
+    for i, row in enumerate(context.table):
+        assert reviews[i].find_by_css('.rating').first.text.count(u"\u2605") == int(row['rating'])
+        if row['comment']:
+            assert row['comment'] == reviews[i].find_by_tag('strong').first.text
+        assert reviews[i].find_by_tag('footer').first.text.startswith(row['user'])
+
+
+@step('list contains "{count:n}" review')
+def step_impl(context, count):
+    assert count == len(context.browser.find_by_css('blockquote.review'))
+
+
+@step('There are "{count:n}" reviews')
+def step_impl(context, count):
+    from distributors.models import CarShopReview
+    assert count == CarShopReview.objects.count()
+
+
+@given("I'm not logged in")
+def step_impl(context):
+    context.browser.visit(context.get_url('logout')+'?next=/distributors/')
+    assert context.browser.is_text_present('Login')
+
+
+@given('Exists review at dealership "{shop_name}" by "{username}"')
+def step_impl(context, shop_name, username):
+    from distributors.models import Person
+    user = Person.objects.get(name=username)
+    from distributors.models import CarShop
+    shop = CarShop.objects.get(shopName=shop_name)
+    from distributors.models import CarShopReview
+    for row in context.table:
+        review = CarShopReview(shopName=shop, user=user)
+        for heading in row.headings:
+            setattr(review, heading, row[heading])
+        review.save()
